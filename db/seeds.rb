24c626308802c811a12efcf7f6523f22613249ab
paul lahana <paul.lahana@gmail.com>
2019-11-14 08:47:46 +0100

    restaurant list in a dflex

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+require 'faker'
+
+10.times do
+  restaurantname = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  phone_number = Faker::PhoneNumber.phone_number_with_country_code
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  Restaurant.create(name: restaurantname, address: address, phone_number: phone_number, category: category)
+end
+
+  # 5.times do
+  #   content = Faker::Restaurant.review
+  #   rating = Faker::Number.within(range: 1..5)
+  #   Review.create(content: content, rating: rating)
+  # end
